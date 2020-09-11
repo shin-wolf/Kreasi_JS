7ac46ca74522f4a268177ac036b63f63269b76c9
shin-wolf <amonsefra13@gmail.com>
2020-09-11 20:43:19 +0700

    kreasi_JS_css

---
+++ java.js
@@ -0,0 +1,5 @@
+// membuat kreasi dengan CSS javascript
+
+console.log("%c Stop", "background : red ; color :white ", "Red Ligth");
+console.log("%c Wait ", "background : yellow ; color : black", "Yellow Ligth");
+console.log("%c Go", " background : green ; color : white ", " Green light ");
